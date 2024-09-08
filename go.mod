75602db7c2fa0fe5172e1f80a51a13a4ac5fffd3
Nick Van Wiggeren <nick@planetscale.com>
2024-09-07 17:32:04 -0700

    bench numbers

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/nickvanw/dbload
 
-go 1.16
+go 1.22
 
 require github.com/go-sql-driver/mysql v1.6.0
--- main.go
+++ main.go
@@ -15,17 +15,19 @@ import (
 
 /*
 CREATE TABLE `data` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `host` varchar(100) DEFAULT NULL,
-  `data` varchar(100) DEFAULT NULL,
-  `now` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+
+	`id` int NOT NULL AUTO_INCREMENT,
+	`host` varchar(100) DEFAULT NULL,
+	`data` varchar(100) DEFAULT NULL,
+	`now` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`)
+
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 */
-var query string = "INSERT INTO data (host, data) VALUES (?, ?)"
+var query string = "INSERT INTO data (host, data) VALUES (?, ?), (?, ?), (?, ?), (?, ?), (?, ?), (?, ?), (?, ?), (?, ?), (?, ?), (?, ?)"
 
 func main() {
-	if err := realMain(5*time.Second, 25); err != nil {
+	if err := realMain(50*time.Millisecond, 20); err != nil {
 		fmt.Printf("error running realMain: %s\n", err)
 		os.Exit(1)
 	}
@@ -42,10 +44,13 @@ func realMain(delay time.Duration, parallelism int) error {
 		return err
 	}
 
+	res := make(chan int64, 100)
+	go bench(res)
+
 	for i := 0; i < parallelism; i++ {
 		fmt.Printf("launching insert loop %d\n", i)
 		go func() {
-			if err := insertLoop(delay, dsn, h); err != nil {
+			if err := insertLoop(delay, dsn, h, res); err != nil {
 				fmt.Println(err)
 				os.Exit(1)
 			}
@@ -68,7 +73,7 @@ func realMain(delay time.Duration, parallelism int) error {
 	return nil
 }
 
-func insertLoop(delay time.Duration, dsn, hostname string) error {
+func insertLoop(delay time.Duration, dsn, hostname string, res chan int64) error {
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		return err
@@ -82,13 +87,18 @@ func insertLoop(delay time.Duration, dsn, hostname string) error {
 	if err != nil {
 		return err
 	}
+
 	for {
-		data := randData(20)
-		fmt.Printf("inputting id=%s, data=%s\n", hostname, data)
-		if _, err := q.Exec(hostname, data); err != nil {
+		data := []any{}
+		for range 10 {
+			data = append(data, hostname, randData(100))
+		}
+		s := time.Now()
+		if _, err := q.Exec(data...); err != nil {
 			fmt.Printf("error inserting data: %s", err)
 			return err
 		}
+		res <- time.Since(s).Milliseconds()
 		time.Sleep(delay)
 	}
 }
@@ -101,4 +111,18 @@ func randData(n int) string {
 		b[i] = letterBytes[rand.Int63()%int64(len(letterBytes))]
 	}
 	return string(b)
+}
+
+func bench(input chan int64) {
+	var total int64
+	var num int64
+	for {
+		bench := <-input
+		total += bench
+		num += 1
+
+		if num%100 == 0 {
+			fmt.Printf("total inserts: %d, avg time: %dms\n", num, total/num)
+		}
+	}
 }
